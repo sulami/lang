a6beae21e871f5bfa8197da98612572f051dfa73
Robin Schroer <sulami@peerwire.org>
2020-04-05 15:12:07 +0200

    Refine the test code a bit

--- tests/test.lisp
+++ tests/test.lisp
@@ -1,11 +1,13 @@
 (printf "Hello from Lisp!\n")
 
+(printf "Printing some unicode: Hélèneちゃんは%sですね\n" "可愛い")
+
 (printf "An integer is: %d\n" 42)
 
 (printf "A float is: %f\n" 3.14159)
 
 (printf "A boolean is: %d or %d\n" true false)
 
-(if true
+(if (random_bool)
     (printf "If is true\n")
     (printf "If is false\n"))
