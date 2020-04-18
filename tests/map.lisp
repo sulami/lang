758dc54032b444a20750d66ea0fa4103e516df17
Robin Schroer <sulami@peerwire.org>
2020-04-18 13:18:16 +0200

    Prettify the map test

--- tests/map.lisp
+++ tests/map.lisp
@@ -1,28 +1,24 @@
 ;; This implements `map' in code. Tests first-class functions,
 ;; cons-lists, and recursion.
 
-;; Auxiliary functions
-
 (defun nil? (x)
   (c/value_equal nil x))
 
-(defun reverse* (from to)
-  (if (nil? from)
-      to
-      (recur (c/cdr from) (c/cons (c/car from) to))))
-
 (defun reverse (l)
-  (reverse* l nil))
-
-(defun map* (f xs acc)
-  (if (nil? xs)
-      acc
-      (recur f
-             (c/cdr xs)
-             (c/cons (f (c/car xs)) acc))))
+  (let ((reverse* (lambda (from to)
+                    (if (nil? from)
+                        to
+                        (recur (c/cdr from) (c/cons (c/car from) to))))))
+    (reverse* l nil)))
 
 (defun map (f l)
-  (reverse (map* f l nil)))
+  (let ((map* (lambda (f from to)
+                (if (nil? from)
+                    to
+                    (recur f
+                           (c/cdr from)
+                           (c/cons (f (c/car from)) to))))))
+    (reverse (map* f l nil))))
 
 (let ((list (c/cons 1 (c/cons 2 (c/cons 3 nil))))
       (double (lambda (x) (* 2 x))))
